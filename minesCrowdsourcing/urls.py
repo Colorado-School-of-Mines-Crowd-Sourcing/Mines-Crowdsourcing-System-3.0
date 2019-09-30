612cf3e2849d27b86f9b89b8d887cb4ac1e82cec
Gazi Mahbub-Morshed <gmorshed@Gazis-MacBook-Pro.local>
2019-09-29 21:20:26 -0600

    mapped to heroku database

--- minesCrowdsourcing/urls.py
+++ minesCrowdsourcing/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobPosting/', include('jobPosting.urls'))
+    path('jobPosting/', include('jobPosting.urls')),
+    path('participant/', include('participant.urls'))
 ]
