a4090249b37d15622bc79d0af42c9687578d8f71
Gazi Mahbub-Morshed <gmorshed@csm-wl-dhcp-78-11.mines.edu>
2019-10-01 11:58:50 -0600

    added more tables

--- participant_homepage/models.py
+++ participant_homepage/models.py
@@ -11,17 +11,31 @@ class Users(AbstractBaseUser):
     email = models.CharField(max_length=50, blank=False)
     CWID = models.IntegerField(unique=True, blank=False)
     authorized_requester = models.BooleanField(blank=False, default=False)
-    reward_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    reward_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+
+class ParticipantCompletedTasks(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    task_ID = models.IntegerField(blank=False)
+
+
+class ActiveTaskID(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    task_ID = models.IntegerField(blank=False)
 
 
 class Task(models.Model):
     task_ID = models.AutoField(primary_key=True, blank=False)
     link_to = models.CharField(max_length=50, blank=False)
-    reward_amount = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
-    min_participant_req = models.IntegerField(max_length=5)
+    reward_amount = models.DecimalField(max_digits=5, decimal_places=2, blank=False, default=0.00)
+    min_participant_req = models.IntegerField
     title = models.CharField(max_length=100, blank=False)
     payment_index = models.IntegerField(blank=False)
     description = models.CharField(max_length=1024, blank=False)
     posted_date = models.DateField(auto_now_add=True, blank=False)
-    end_date = models.DateField(auto_now_add=True, blank=False)
+    end_date = models.DateField(blank=False)
 
+
+class Tag(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=20, blank=False)
