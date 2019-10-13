864135aad1d41a6e7c7af55b9aba6fdb7aaf043e
unknown <leo.chely@live.fr>
2019-10-13 16:36:23 -0600

    No error message on form submission (random user created as a temporary fix until registration is implemented)

--- requester/views.py
+++ requester/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.apps import apps
 
 from .forms import CreateJob
 
+# TODO: Remove random import when user authentication is done
+import random, string
+
+# Logger
+import logging
+
+logger = logging.getLogger(__name__)
 def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -11,9 +19,12 @@ def index(request):
         # check whether it's valid:
         if form.is_valid():
             new_job = form.save(commit=False)
-            new_job.requester = request.user
+            random_username = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+            user = apps.get_model('participant', 'User').objects.create_user(random_username, 'test', 'test@test.mines', random.randint(0, 2000), True)
+            #new_job.requester = request.user
+            new_job.requester = user
             new_job.save()
-            messages.success(request, "Your task has been submitted for review.")
+            logger.info(request, "Your task has been submitted for review.")
             return HttpResponseRedirect('/')
 
     # if a GET (or any other method) we'll create a blank form
