8e9b95c4318be3b53c08dab6f117d7cc7ff88427
Gazi Mahbub-Morshed <gmorshed@csm-wl-dhcp-69-228.mines.edu>
2019-10-17 11:59:10 -0600

    implimenting participant backend queries

--- participant/urls.py
+++ participant/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='participant_index'),
     path('all_task/', views.all_available_tasks, name='participant_all_tasks'),
-    path('completed_tasks/', views.index, name='participant_completed_tasks'),
+    path('completed_tasks/', views.completed_tasks, name='participant_completed_tasks'),
 ]
--- participant/views.py
+++ participant/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from participant.models import *
 from django.http import HttpResponse
 
 
@@ -7,4 +8,17 @@ def index(request):
 
 
 def all_available_tasks(request):
-    return render(request, 'participant/all_tasks.html')
+    return render(request, 'participant/all_tasks.html', {
+        'all_tasks': Task.objects.filter(is_posted=True)})
+
+
+def completed_tasks(request):
+    user = request.user
+    if user.is_anonymous:
+        return render(request, 'participant/completed_task.html')
+    else:
+        all_tasks = ParticipantCompletedTask.objects.filter(
+            user=user
+        )
+        return render(request, 'participant/completed_task.html', {
+            'all_tasks': all_tasks})
