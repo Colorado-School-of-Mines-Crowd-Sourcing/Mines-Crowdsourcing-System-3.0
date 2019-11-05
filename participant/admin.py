1901abb9a4b60c608eee3c1dc3a1c205312b7a50
Léo Chely <31514354+leochely@users.noreply.github.com>
2019-11-05 11:29:39 -0700

    Not processed action

--- participant/admin.py
+++ participant/admin.py
@@ -27,13 +27,17 @@ class ParticipantCompletedTaskAdmin(admin.ModelAdmin):
     pass
 
 
-# Transactions processing action 
+# Transactions processing actions
 def make_processed(modeladmin, request, queryset):
     queryset.update(processed=True)
 make_processed.short_description = "Mark selected transactions as processed"
+
+def make_not_processed(modeladmin, request, queryset):
+    queryset.update(processed=False)
+make_not_processed.short_description = "Mark selected transactions as not processed"
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'processed']
     list_filter = ['processed']
-    actions = [make_processed]
+    actions = [make_processed, make_not_processed]
