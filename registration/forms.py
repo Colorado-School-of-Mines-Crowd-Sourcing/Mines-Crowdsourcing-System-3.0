bbbfe40372520befa483eed1d890a3f5a73c4f22
Léo Chely <31514354+leochely@users.noreply.github.com>
2019-11-11 11:31:05 -0700

    Added crispy form + password disclaimer in registration form

--- registration/forms.py
+++ registration/forms.py
@@ -5,3 +5,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = apps.get_model('participant', 'User')
         fields = ['multipass_username', 'name', 'CWID']
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].help_text = 'You should NOT use your multipass password'
