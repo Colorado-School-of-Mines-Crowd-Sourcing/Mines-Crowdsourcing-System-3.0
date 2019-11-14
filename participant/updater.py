ce98f068ebb3f367d0494f81bbcc2a92c38987bf
Gazi Mahbub-Morshed <gmorshed@csm-wl-dhcp-73-2.mines.edu>
2019-11-14 11:49:59 -0700

    expired task cleaning job finalized to run every 24 hours

--- participant/updater.py
+++ participant/updater.py
@@ -1,17 +1,22 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 import pytz
-from participant.models import Task
 
 
-def myjob():
-    print("Daily expired job clean up")
-    current_time = datetime.now(pytz.timezone('America/Denver'))
-    print("time now: ", current_time)
-
+def daily_expired_task_cleaner():
+    date_of_today = datetime.date(datetime.now(pytz.timezone('America/Denver')))
+    try:
+        from participant.models import Task
+        all_active_tasks = Task.objects.filter(status=Task.ACTIVE)
+        for task in all_active_tasks:
+            if task.end_date < date_of_today:
+                task.status = Task.COMPLETED
+                task.save()
+    except:
+        pass
 
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(myjob, 'interval', seconds=10)
+    scheduler.add_job(daily_expired_task_cleaner, 'interval', hours=24)
     scheduler.start()
