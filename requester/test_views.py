598ef88f04c113d0e1a67b0be5981a4dc0e87661
unknown <leo.chely@live.fr>
2020-01-25 07:34:24 -0700

    Refactor + 404 test for approval

--- requester/test_views.py
+++ requester/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.apps import apps
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from requester.views import *
 
@@ -21,12 +21,26 @@ class Create(TestCase):
 class SeeTasks(TestCase):
     def setUp(self):
         self.client = Client()
-        self.create_url = reverse('requester_tasks')
+        self.see_task_url = reverse('requester_tasks')
 
     def test_see_GET(self):
         print('**************test_see_GET()******************')
 
-        response = self.client.get(self.create_url)
+        response = self.client.get(self.see_task_url)
         print('Response status code : ' + str(response.status_code))
 
         self.assertEquals(response.status_code, 200)
+
+
+class ApproveContributors(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_see_GET_404(self):
+        print('**************test_see_GET()******************')
+
+        self.approve_url = reverse_lazy('contributor_approval', args=[1])
+        response = self.client.get(self.approve_url)
+        print('Response status code : ' + str(response.status_code))
+
+        self.assertEquals(response.status_code, 404)
